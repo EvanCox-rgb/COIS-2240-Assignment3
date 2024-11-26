e6f96a57b9cc2abe1779d074691a72d84686faac
EvanCox-rgb <ecox2123@gmail.com>
2024-11-25 20:44:48 -0500

    Task2.2
    only Transaction file is updates. Includes saveTransaction() method. It will keep previous logs... well... logged. uses while loop to read and remember old logs then adds new transaction details and updates.

--- assignment3_source/src/Transaction.java
+++ assignment3_source/src/Transaction.java
@@ -1,13 +1,49 @@
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
-
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
 public class Transaction {
 
+	ArrayList<String> logs;
     // Perform the borrowing of a book
 	private static Transaction instance;		//static reference for our singleton design
 	public Transaction() {}						//non static constructor
 	
-	
+	public void saveTransaction(String transaction){
+		try {
+			ArrayList<String> prevLogs = new ArrayList<String>();
+			
+			BufferedReader reader = new BufferedReader(new FileReader("transactions.txt"));
+			String line;
+			while((line = reader.readLine()) != null){
+				prevLogs.add(line);
+				//System.out.println(line);
+				logs = prevLogs;
+			}
+			reader.close();
+			
+			if (logs!=null) {
+				BufferedWriter writer = new BufferedWriter(new FileWriter("transactions.txt"));
+				for (String logLine: logs) {
+					writer.write(logLine + "\n");
+				}
+				writer.write(transaction + "\n");
+				writer.close();
+			}
+			else {
+				BufferedWriter writer = new BufferedWriter(new FileWriter("transactions.txt"));
+				writer.write(transaction + "\n");
+				writer.close();
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	
     public boolean borrowBook(Book book, Member member) {
@@ -16,6 +52,7 @@ public class Transaction {
             member.borrowBook(book); 
             String transactionDetails = getCurrentDateTime() + " - Borrowing: " + member.getName() + " borrowed " + book.getTitle();
             System.out.println(transactionDetails);
+            saveTransaction(transactionDetails);
             return true;
         } else {
             System.out.println("The book is not available.");
@@ -30,6 +67,7 @@ public class Transaction {
             book.returnBook();
             String transactionDetails = getCurrentDateTime() + " - Returning: " + member.getName() + " returned " + book.getTitle();
             System.out.println(transactionDetails);
+            saveTransaction(transactionDetails);
         } else {
             System.out.println("This book was not borrowed by the member.");
         }
